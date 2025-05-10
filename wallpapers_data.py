476b5c6bdd90fb7dc519a5ac6f820d4310a4d482
izonApp <hitechmarkettrading@gmail.com>
2025-05-10 14:22:43 +0100

    Update wallpapers_data.py

--- wallpapers_data.py
+++ wallpapers_data.py
@@ -1,202 +1,202 @@
 wallpapers = [
    {
-            "wid": "1",
+            "wid": 1,
             "cid": "5",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/29.jpg",
             "ec": "1",
             "pm": "1"
         },
         {
-            "wid": "2",
+            "wid": 2,
             "cid": "5",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/28.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "3",
+            "wid": 3,
             "cid": "5",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/27.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "4",
+            "wid": 4,
             "cid": "5",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/26.jpg",
             "ec": "1",
             "pm": "1"
         },
         {
-            "wid": "5",
+            "wid": 5,
             "cid": "5",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/25.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "6",
+            "wid": 6,
             "cid": "5",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/24.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "7",
+            "wid": 7,
             "cid": "5",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/23.jpg",
             "ec": "1",
             "pm": "1"
         },
         {
-            "wid": "8",
+            "wid": 8,
             "cid": "4",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/22.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "9",
+            "wid": 9,
             "cid": "4",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/21.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "10",
+            "wid": 10,
             "cid": "4",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/20.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "11",
+            "wid": 11,
             "cid": "4",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/19.jpg",
             "ec": "1",
             "pm": "1"
         },
         {
-            "wid": "12",
+            "wid": 12,
             "cid": "4",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/18.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "13",
+            "wid": 13,
             "cid": "3",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/17.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "14",
+            "wid": 14,
             "cid": "3",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/16.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "15",
+            "wid": 15,
             "cid": "3",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/15.jpg",
             "ec": "1",
             "pm": "1"
         },
         {
-            "wid": "16",
+            "wid": 16,
             "cid": "3",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/14.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "17",
+            "wid": 17,
             "cid": "3",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/13.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "18",
+            "wid": 18,
             "cid": "2",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/9.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "19",
+            "wid": 19,
             "cid": "2",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/8.jpg",
             "ec": "1",
             "pm": "1"
         },
         {
-            "wid": "20",
+            "wid": 20,
             "cid": "2",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/7.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "21",
+            "wid": 21,
             "cid": "2",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/12.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "22",
+            "wid": 22,
             "cid": "2",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/11.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "23",
+            "wid": 23,
             "cid": "2",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/10.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "24",
+            "wid": 24,
             "cid": "1",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/6.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "25",
+            "wid": 25,
             "cid": "1",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/5.jpg",
             "ec": "1",
             "pm": "1"
         },
         {
-            "wid": "26",
+            "wid": 26,
             "cid": "1",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/4.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "27",
+            "wid": 27,
             "cid": "1",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/3.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "28",
+            "wid": 28,
             "cid": "1",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/2.jpg",
             "ec": "0",
             "pm": "0"
         },
         {
-            "wid": "29",
+            "wid": 29,
             "cid": "1",
             "wlink": "http:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/1.jpg",
             "ec": "1",
