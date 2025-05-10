6ab762fd0f09223ead106bdc1d3cfbe597be65d7
izonApp <hitechmarkettrading@gmail.com>
2025-05-10 13:25:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+import os
 from flask import Flask, request, jsonify
 from wallpapers_data import wallpapers
 
@@ -9,4 +10,5 @@ def get_wallpapers():
     return jsonify({"status": True, "data": wallpapers})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
