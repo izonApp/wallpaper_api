b50fdff11b7409bc868ce9866edf2523d058eb81
izonApp <hitechmarkettrading@gmail.com>
2025-05-10 12:50:23 +0100

    Create wallpapers_data.py

---
+++ wallpapers_data.py
@@ -0,0 +1,17 @@
+wallpapers = [
+    {
+        "wid": 1,
+        "cid": "2",
+        "wlink": "http://varnisofttech.in/4kwallpaper/4K/1.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 2,
+        "cid": "2",
+        "wlink": "http://varnisofttech.in/4kwallpaper/4K/2.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    # Add more if needed
+]
