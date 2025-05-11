c0fc17c740258d62ec5269656eb39c4abe88abba
izonApp <hitechmarkettrading@gmail.com>
2025-05-11 12:19:07 +0100

    Update wallpapers_data.py

--- wallpapers_data.py
+++ wallpapers_data.py
@@ -1,206 +1,206 @@
 wallpapers = [
     {
-            "wid": 1,
-            "cid": "5",
-            "wlink": "https://tinyurl.com/sdfaw321",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 2,
-            "cid": "5",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/24.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 3,
-            "cid": "5",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/23.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 4,
-            "cid": "2",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/8.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 5,
-            "cid": "2",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/9.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 6,
-            "cid": "2",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/11.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 7,
-            "cid": "2",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/12.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 8,
-            "cid": "1",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/5.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 9,
-            "cid": "1",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/6.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 10,
-            "cid": "1",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/1.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 11,
-            "cid": "1",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/2.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 12,
-            "cid": "1",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/3.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 13,
-            "cid": "4",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/22.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 14,
-            "cid": "4",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/21.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 15,
-            "cid": "4",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/20.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 16,
-            "cid": "4",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/19.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 17,
-            "cid": "4",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/4\/18.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 18,
-            "cid": "3",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/17.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 19,
-            "cid": "3",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/16.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 20,
-            "cid": "3",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/15.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 21,
-            "cid": "3",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/14.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 22,
-            "cid": "3",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/3\/13.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 23,
-            "cid": "2",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/7.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 24,
-            "cid": "2",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/2\/10.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 25,
-            "cid": "1",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/1\/4.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 26,
-            "cid": "5",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/26.jpg",
-            "ec": "1",
-            "pm": "1"
-        },
-        {
-            "wid": 27,
-            "cid": "5",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/27.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 28,
-            "cid": "5",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/28.jpg",
-            "ec": "0",
-            "pm": "0"
-        },
-        {
-            "wid": 29,
-            "cid": "5",
-            "wlink": "https:\/\/aniwall.infinityfreeapp.com\/Wallpapers\/5\/29.jpg",
-            "ec": "0",
-            "pm": "0"
-        }
+        "wid": 1,
+        "cid": "1",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961294/wallpapers/1/myxoc2tgsqazdwallypf.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 2,
+        "cid": "1",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961295/wallpapers/1/ecliwsb2kdqva1cph2q8.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 3,
+        "cid": "1",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961296/wallpapers/1/ygcwu4soytdalktk4ncl.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 4,
+        "cid": "1",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961297/wallpapers/1/gcy6h5uda0lj5nol0rfp.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 5,
+        "cid": "1",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961299/wallpapers/1/dtmivibypvqllpzlskdn.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 6,
+        "cid": "1",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961300/wallpapers/1/buvi5gj8tq2fbey3zgdo.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 7,
+        "cid": "2",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961301/wallpapers/2/dis83mdvvbkdajzow7wb.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 8,
+        "cid": "2",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961302/wallpapers/2/jyd6hn1zsgas79g4b9ik.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 9,
+        "cid": "2",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961303/wallpapers/2/gejqcefz50neekgewzpw.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 10,
+        "cid": "2",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961303/wallpapers/2/g2ftdjozsi7akxphwpjz.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 11,
+        "cid": "2",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961304/wallpapers/2/lcrvvj235y9kh7hw814w.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 12,
+        "cid": "2",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961305/wallpapers/2/tmqzc78kemiom7hcexn6.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 13,
+        "cid": "3",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961306/wallpapers/3/u9tmirb2zzxntjy0obg6.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 14,
+        "cid": "3",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961307/wallpapers/3/riypfbokvex7cbjpfdsr.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 15,
+        "cid": "3",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961308/wallpapers/3/orpytnnlqxndvjucjx1f.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 16,
+        "cid": "3",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961308/wallpapers/3/creckmd6zfpa7rufprej.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 17,
+        "cid": "3",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961309/wallpapers/3/ugfeocayxnvzftrmfi8c.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 18,
+        "cid": "4",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961310/wallpapers/4/q0y3wu40fksjrplqocko.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 19,
+        "cid": "4",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961311/wallpapers/4/e4a7j4djhdteo6rro5k7.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 20,
+        "cid": "4",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961312/wallpapers/4/woyf8v3er78hpkscmrtn.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 21,
+        "cid": "4",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961313/wallpapers/4/se7qy3lwrrespsu4rq1k.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 22,
+        "cid": "4",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961314/wallpapers/4/fbnjhjnpstvmvmvdwu8e.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 23,
+        "cid": "5",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961314/wallpapers/5/eya1d4iub89x1yiwjr0w.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 24,
+        "cid": "5",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961315/wallpapers/5/fzg5dzuwio8trgnlnkdt.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 25,
+        "cid": "5",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961316/wallpapers/5/lya1mvm0qjey3vq4e0v0.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 26,
+        "cid": "5",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961317/wallpapers/5/iclh85qesgt68wsn3dpt.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 27,
+        "cid": "5",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961317/wallpapers/5/dhadr4xgrx2akwher1gz.jpg",
+        "ec": "1",
+        "pm": "1"
+    },
+    {
+        "wid": 28,
+        "cid": "5",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961318/wallpapers/5/nkaudazgjtwtx94gxp9i.jpg",
+        "ec": "0",
+        "pm": "0"
+    },
+    {
+        "wid": 29,
+        "cid": "5",
+        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1746961319/wallpapers/5/bbltsohyh4yirljtjcwt.jpg",
+        "ec": "1",
+        "pm": "1"
+    }
     # Add more if needed
 ]
