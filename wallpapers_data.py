b2723cf420baa25a3e40c00c3da686c90d2f565a
izonApp <hitechmarkettrading@gmail.com>
2025-05-15 16:07:37 +0100

    Update wallpapers_data.py

--- wallpapers_data.py
+++ wallpapers_data.py
@@ -2,7 +2,7 @@ wallpapers = [
     {
         "wid": 1,
         "cid": "1",
-        "wlink": "https://res.cloudinary.com/dha74cfvx/image/upload/v1747321049/1_g4fzqf.jpg",
+        "wlink": "https://drive.google.com/file/d/1zaYQStFyGxaIpoocEv7DH3WQ85jXX-1n/view?usp=drive_link",
         "ec": "0",
         "pm": "0"
     },
